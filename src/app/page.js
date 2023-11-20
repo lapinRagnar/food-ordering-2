e9c15a179ad43e5f0c9734ce674810841be6c364
lapinRagnar <lapinragnar@gmail.com>
2023-11-20 20:28:03 +0100

    html header

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>bonjour</div>
+    <>
+      <header>
+        <Link href="" className="text-red-700 font-bold text-5xl">pizza lapinRagnar</Link>
+        <nav>
+          <Link href={''}>Accueil</Link>
+          <Link href={''}>Menu</Link>
+          <Link href={''}>A propos</Link>
+          <Link href={''}>A Contact</Link>
+          <Link href={''}>A Login</Link>
+
+        </nav>
+      </header>
+    </>
   )
 }
