0307eb6a434569db445d8767b51dd263190fa00c
lapinRagnar <lapinragnar@gmail.com>
2023-11-21 09:01:23 +0100

    le header

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className='max-w-6xl border mx-auto'>
+        <main className='max-w-7xl mx-auto'>
           {children}
         </main>
 
