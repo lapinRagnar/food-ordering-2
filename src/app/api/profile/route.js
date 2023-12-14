129933a2de4591287355d72320368a679ede55d3
lapinRagnar <lapinragnar@gmail.com>
2023-12-14 21:18:33 +0100

    suite éditer user par _id

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -5,35 +5,69 @@ import { User } from "@/models/User"
 import { UserInfo } from "@/models/UserInfo"
 
 
+
 export async function PUT(req) {
   
   mongoose.connect(process.env.MONGO_URL)
   const data = await req.json()
 
-  const {name, imageId, ...otherUserInfo} = data 
+  const {_id, name, imageId, ...otherUserInfo} = data 
+
+  console.log('_id dans api profile', _id)
 
   const session = await getServerSession(options)
-
-
-
   const email = session.user.email
 
-
-  const result = await User.updateOne({email}, {name, imageId})
-
-
-  const result2 = await UserInfo.findOneAndUpdate({email}, otherUserInfo, {upsert: true})
+  let filter = {}
   
+  if (_id) {
+    filter = {_id}
+  } else {
+    filter = {email}
+  }
+
+
+  const user = await User.findOne(filter)
+  
+  const result = await User.updateOne(filter, {name, imageId})
+  console.log("mettre à jour name et imageId", result)
+  const result2 = await UserInfo.findOneAndUpdate({email: user.email}, otherUserInfo, {upsert: true})
+  console.log("mettre à jour UserInfo", result2);
+
   
   return Response.json(true)
 }
 
-export async function GET() {
+export async function GET(req) {
   mongoose.connect(process.env.MONGO_URL)
-  const session = await getServerSession(options)
-  const email = session.user.email
 
-  const user = await User.findOne({email}).lean()
-  const userInfo = await UserInfo.findOne({email}).lean()
+  const url = new URL(req.url)
+  console.log("api get url ", url)
+
+  const _id = url.searchParams.get('_id')
+  console.log('id dans searchParams', _id);
+
+  let filterUser = {}
+
+  if (_id) {
+    filterUser = {_id}
+    // const userInfo = await UserInfo.findOne({email: user.email}).lean()
+
+  } else {
+    const session = await getServerSession(options)
+    const email = session.user.email
+
+    if (!email) {
+      return Response.json({error: 'Pas connecté!'})
+    }
+
+    filterUser = {email}
+    
+  }
+  
+  const user = await User.findOne(filterUser).lean()
+  const userInfo = await UserInfo.findOne({email: user.email}).lean()
+
   return Response.json({...user, ...userInfo})
+
 }
--- src/app/users/[id]/page.js
+++ src/app/users/[id]/page.js
@@ -1,14 +1,38 @@
 'use client'
 
 import { useProfile } from "@/app/components/UseProfile"
+import UserForm from "@/app/components/layout/UserForm"
 import UserTabs from "@/app/components/layout/UserTabs"
+import { useParams } from "next/navigation"
+import { useEffect, useState } from "react"
 
 const EditUserPage = () => {
   
   const {data: profileData, loading: profileLoading} = useProfile()
+  const [user, setUser] = useState(null)
 
+  const {id} = useParams()
+  console.log('edit user id', id)
 
+  useEffect(() => {
+    fetch('/api/profile?_id=' + id).then(res => {
+      res.json().then(user => {
 
+        setUser(user)
+      })
+    })
+  }, [])
+
+  const handleSaveButtonClick = (e, data) => {
+    e.preventDefault()
+    fetch('/api/profile', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({...data, _id: id})
+    })
+  }
 
 
   if (profileLoading) {
@@ -36,11 +60,27 @@ const EditUserPage = () => {
 
   return (
     
-    <section className="mt-8 min-h-[500px] max-w-2xl mx-auto">
+    <section className="mt-8 min-h-[580px] max-w-2xl mx-auto">
 
       <UserTabs admin={true} />
 
-      <div>salut</div>
+      <div 
+        className="
+          min-w-[800px] mx-auto  
+          bg-[#4e9b65] 
+          p-2
+          shadow-lg shadow-slate-600
+          bg-gradient-to-r from-green-200 via-green-400 to-purple-700
+          rounded-sm
+        "
+      >
+
+        <UserForm
+          user={user}
+          onSave={handleSaveButtonClick}
+        />
+
+      </div>
 
     </section>
   )
